b707534856377501dcc8dd915eb6c059d43ccaf1
localusers user <dgeeraer@m7.iihe.ac.be>
2026-02-16 16:18:17 +0100

    add: entrypoint.sh

--- Container/entrypoint.sh
+++ Container/entrypoint.sh
@@ -2,18 +2,6 @@
 set -e
 
 # -----------------------------
-# Set paths for simulation tools
-# -----------------------------
-# EcoMug headers
-export CPLUS_INCLUDE_PATH=/opt/EcoMug:$CPLUS_INCLUDE_PATH
-
-# CRY, PUMAS, Turtle binaries
-export PATH=/opt/CRY/bin:/opt/PUMAS/bin:/opt/Turtle/examples:$PATH
-
-# Optional: Turtle library path if needed
-export LD_LIBRARY_PATH=/opt/Turtle/lib:$LD_LIBRARY_PATH
-
-# -----------------------------
 # Welcome message
 # -----------------------------
 echo " ~~~~~~~  Welcome to the Muraves Geant4 Simulation Container 0.0 ~~~~~~~~  "
@@ -31,7 +19,6 @@ echo " ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  "
 echo "Tools installed in /opt:"
 ls /opt
 
-
 # -----------------------------
 # Source environment for Geant4 via geant4-config
 # -----------------------------
@@ -40,11 +27,9 @@ if command -v geant4-config >/dev/null 2>&1; then
     export PATH=$G4INSTALL/bin:$PATH
     export LD_LIBRARY_PATH=$(geant4-config --libdir):$LD_LIBRARY_PATH
     export CPLUS_INCLUDE_PATH=$(geant4-config --includedir):$CPLUS_INCLUDE_PATH
+    export GEANT4_DATA_DIR=/root/geant4/data
     echo "[INFO] Geant4 environment loaded. G4INSTALL=$G4INSTALL"
-    #echo "[DEBUG] G4INSTALL=$G4INSTALL"
-    #echo "[DEBUG] PATH=$PATH"
-    #echo "[DEBUG] LD_LIBRARY_PATH=$LD_LIBRARY_PATH"
-    #echo "[DEBUG] CPLUS_INCLUDE_PATH=$CPLUS_INCLUDE_PATH"
+    echo "Working with Geant4 version: $(geant4-config --version)"
 else
     echo "[WARNING] geant4-config not found. Geant4 environment not loaded."
 fi
@@ -58,16 +43,16 @@ else
     echo "Detected existing Git repo in /workspace."
 fi
 
-echo "[INFO] Container ready."
 # -----------------------------
 # Launch interactive shell
 # -----------------------------
-
-# -----------------------------
-# Container identity & custom prompt
-# -----------------------------
-export CONTAINER_NAME="muraves-sim"
-export CLUSTER_USER="${USER}"   # host username
-export PS1="[\[\e[36m\]${CONTAINER_NAME}\[\e[0m\] | \[\e[32m\]${CLUSTER_USER}\[\e[0m\] | \[\e[34m\]\w\[\e[0m\]]\$ "
-
+<<<<<<< Updated upstream:docker-singularity/entrypoint.sh
+echo "[INFO] Container ready. Tools available in /opt"
 exec bash -l
+
+
+=======
+
+echo "Current working directory: $PWD"
+exec bash -l
+>>>>>>> Stashed changes:Container/entrypoint.sh
